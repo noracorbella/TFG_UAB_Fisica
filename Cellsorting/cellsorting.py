0ee7015515041041a77cd22453a754db9c513f83
noracorbella <100218149+noracorbella@users.noreply.github.com>
2025-02-03 12:01:31 +0100

    Delete Cellsorting directory

--- Cellsorting/cellsorting.py
+++
@@ -1,6 +0,0 @@
-from cc3d import CompuCellSetup   
-from cellsortingSteppables import cellsortingSteppable
-
-
-CompuCellSetup.register_steppable(steppable=cellsortingSteppable(frequency=1))
-CompuCellSetup.run()
