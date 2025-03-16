a0ba8c0212d1fef890c6fa2694e49fdbcc6de5b2
noracorbella <100218149+noracorbella@users.noreply.github.com>
2025-03-16 16:48:51 +0100

    Add files via upload

---
+++ CompuCell3D/Cellsorting/CellSorting_T10/Simulation/CellSorting.py
@@ -0,0 +1,7 @@
+
+from cc3d import CompuCellSetup 
+
+from CellSortingSteppables import CellSortingSteppable
+CompuCellSetup.register_steppable(steppable=CellSortingSteppable(frequency=1))
+
+CompuCellSetup.run()
