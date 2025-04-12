791e7d7295c6a26c8bcc82a253aa36363e88a1ad
noracorbella <100218149+noracorbella@users.noreply.github.com>
2025-04-12 15:54:35 +0200

    Update Plotter_trajectories_from_xtc.py

--- CompuCell3D/TcellDynabeadSystem/TcellsDynabeadSystem_DBTracker/DBTracker_4/Codes/Plotter_trajectories_from_xtc.py
+++ CompuCell3D/TcellDynabeadSystem/TcellsDynabeadSystem_DBTracker/DBTracker_4/Codes/Plotter_trajectories_from_xtc.py
@@ -1,21 +1,12 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Apr 10 18:04:39 2025
-
-@author: nora
-"""
 
 import MDAnalysis as mda
 import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-traj_file = r"/mnt/c/Users/norac/OneDrive - UAB/Escritorio/uab/5/TFGJordi/ExperimentalData/TcellsDynabeadSystem/DBTracker/DBTracker_4/DB4_traj_unwrapped.xtc"
-
-top_file = r"/mnt/c/Users/norac/OneDrive - UAB/Escritorio/uab/5/TFGJordi/ExperimentalData/TcellsDynabeadSystem/DBTracker/DBTracker_4/DB4_frame0.pdb"
-
-plot = r"/mnt/c/Users/norac/OneDrive - UAB/Escritorio/uab/5/TFGJordi/ExperimentalData/TcellsDynabeadSystem/DBTracker/DBTracker_4/DB4_traj_plot.png"
+traj_file = r"DB4_traj_unwrapped.xtc"
+top_file = r"DB4_frame0.pdb"
+plot = r"DB4_traj_plot.png"
 
 u = mda.Universe(top_file, traj_file)
 
@@ -39,16 +30,13 @@ def plot_traj(data, color, marker, linewidth):
         if x_plot:
             plt.plot(x_plot, y_plot, color=color, marker=marker, linewidth=linewidth)
 
-# Get positions
 positions = []
 for ts in u.trajectory:
-    positions.append(atom.position[:2].copy())  # Only x and y
+    positions.append(atom.position[:2].copy()) 
 
 positions = np.array(positions)
-# Convert to dict format expected by plot_traj
 data = {0: (positions[:, 0], positions[:, 1])}
 
-# Plotting
 plot_traj(data, 'green', None, 1)
 
 plt.grid(True)
