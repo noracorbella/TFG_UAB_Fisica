8422382dfec7bfd57ac3bf1766c38d57c8e04972
noracorbella <100218149+noracorbella@users.noreply.github.com>
2025-04-20 09:33:20 +0200

    Create TcellsDynabeadSystem_DiffusionTracker.py

---
+++ CompuCell3D/TcellDynabeadSystem/TcellsDynabeadSystem_DBTracker/DBTracker_7/Simulation/TcellsDynabeadSystem_DiffusionTracker.py
@@ -0,0 +1,14 @@
+
+from cc3d import CompuCellSetup
+        
+
+
+from TcellsDynabeadSystem_DiffusionTrackerSteppables import CellInitialiser
+from TcellsDynabeadSystem_DiffusionTrackerSteppables import TrajectoryTrackerSteppable
+
+
+CompuCellSetup.register_steppable(steppable=CellInitialiser(frequency=1))
+CompuCellSetup.register_steppable(steppable=TrajectoryTrackerSteppable(frequency=1))
+
+
+CompuCellSetup.run()
