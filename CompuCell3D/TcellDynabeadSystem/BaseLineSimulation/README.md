b373f9ace039c401ab6995247ff0f641365815bc
noracorbella <100218149+noracorbella@users.noreply.github.com>
2025-05-28 16:34:58 +0200

    Delete CompuCell3D/TcellDynabeadSystem/BaseLineSimulation directory

--- CompuCell3D/TcellDynabeadSystem/BaseLineSimulation/README.md
+++
@@ -1,1 +0,0 @@
-First simulation with TCells and Dynabeads, where the motion of the cells is determined by a Brownian ForceField.
--- CompuCell3D/TcellDynabeadSystem/BaseLineSimulation/Simulation/BaseLineSimulation.py
+++
@@ -1,8 +0,0 @@
-
-from cc3d import CompuCellSetup
-from BaseLineSimulationSteppables import CellInitialiser, TCellMotilitySteppable, DynabeadMotilitySteppable
-
-CompuCellSetup.register_steppable(steppable=CellInitialiser(frequency=1))
-CompuCellSetup.register_steppable(steppable=TCellMotilitySteppable(frequency=1))
-CompuCellSetup.register_steppable(steppable=DynabeadMotilitySteppable(frequency=1))
-CompuCellSetup.run()
